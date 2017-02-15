fe581949b5722467fc86ae886215b882dc6656e0
Lin <291587302@qq.com>
2017-02-15 14:55:28 +0800

    创建工程

---
+++ app/src/main/java/gift/lin/lanou/com/text3/MainActivity.java
@@ -0,0 +1,13 @@
+package gift.lin.lanou.com.text3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
